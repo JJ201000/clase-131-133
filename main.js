0d58281ec35b4462c38e555bd120ecbe7108e88d
JJ201000 <102840781+JJ201000@users.noreply.github.com>
2023-02-15 20:30:10 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 img="";
 statusc="";
-
+objectDetector="";
+objectS=[];
 
 
 
@@ -15,28 +16,39 @@ function setup()
 canvas = createCanvas(450, 450);
 canvas.center();
 objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-document.getElementById("status").innerHTML="detectando cosas";
+document.getElementById("mario").innerHTML="detectando cosas";
 
 }
 
 function draw()
 {
-    image(img, 0 , 0 , 450 , 450);
-    fill("#FF0000");
-    text("perro", 45 , 75);
+   image(img, 0 , 0 , 450 , 450);
+   if (statusc != "") 
+   {
+    for ( i = 0; i<objectS.length; i++)
+    {
+   document.getElementById("mario").innerHTML="status:objetos decectados";
+
+
+   fill("#FF0000");
+   percent=floor(objectS[i].confidence * 100)
+    text(objectS[i].label + " " + percent + "%", objectS[i].x + 15 , objectS[i].y + 15);
     noFill();
     stroke("#FF0000");
-    rect(50,60 , 200 , 200);
-
-    fill("#05f0c1");
-    text("gato", 110 , 110);
-    noFill();
-    stroke("#05f0c1");
-    rect(100 , 100 , 200 , 200);
+    rect(objectS[i].x , objectS[i].y , objectS[i].width , objectS[i].haight);
+}
+}
 }
 function modelLoaded(){
     console.log("ha cargado el modelo con exito haora es hora de morir tin")
     statusc=true;
     objectDetector.detect(img, gotResult);
-    
+
+}
+function gotResult(error,results)
+{
+    if (error){
+        console.log(error);
+    }
+    console.log(results)
 }
